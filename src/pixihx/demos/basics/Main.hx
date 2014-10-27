e41be51e2f019e2edcd144ab3a1917d69468f73a
Adi Mora <Adi.Mora@Gamesys.co.uk>
2014-10-27 15:58:04 +0000

    testing indentation

--- src/pixihx/demos/basics/Main.hx
+++ src/pixihx/demos/basics/Main.hx
@@ -1,5 +1,6 @@
 package pixihx.demos.basics;
 
+import pixi.renderers.webgl.WebGLRenderer;
 import pixi.display.Sprite;
 import pixi.display.Stage;
 import pixi.textures.Texture;
@@ -8,34 +9,33 @@ import js.Browser;
 
 class Main {
 
-    var _bunny:Sprite;
-    var _renderer:Dynamic;
-    var _stage:Stage;
+	var _bunny:Sprite;
+	var _renderer:WebGLRenderer;
+	var _stage:Stage;
 
 	public function new() {
 		_stage = new Stage(0x00FF00);
-        _renderer = Detector.autoDetectRenderer(800, 600);
-        Browser.document.body.appendChild(_renderer.view);
-        
-        var texture = Texture.fromImage("assets/basics/bunny.png");
-        _bunny = new Sprite(texture);
-        _bunny.anchor.x = _bunny.anchor.y = 0.5;
-        _bunny.position.x = 400;
-        _bunny.position.y = 300;
-        _bunny.scale.x = _bunny.scale.y = 2;
-        
-        _stage.addChild(_bunny);
-        
-        Browser.window.requestAnimationFrame(cast animate);
+		_renderer = Detector.autoDetectRenderer(800, 600);
+		Browser.document.body.appendChild(_renderer.view);
+
+		_bunny = new Sprite(Texture.fromImage("assets/basics/bunny.png"));
+		_bunny.anchor.set(0.5, 0.5);
+		_bunny.scale.set(2, 2);
+		_bunny.position.x = 400;
+		_bunny.position.y = 300;
+
+		_stage.addChild(_bunny);
+
+		Browser.window.requestAnimationFrame(cast animate);
 	}
 
 	function animate() {
-        Browser.window.requestAnimationFrame(cast animate);
-        _bunny.rotation += 0.1;  
-        _renderer.render(_stage);
-    }
+		Browser.window.requestAnimationFrame(cast animate);
+		_bunny.rotation += 0.1;
+		_renderer.render(_stage);
+	}
 
-    static function main() {
+	static function main() {
 		new Main();
 	}
 }
